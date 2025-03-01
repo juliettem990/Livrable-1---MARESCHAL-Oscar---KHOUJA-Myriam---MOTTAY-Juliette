3acf1058c8001319dd2e89788f8efc7abe507f18
juliettem990 <juliette.mottay@edu.devinci.fr>
2025-03-01 19:37:06 +0100

    Livrable 1

---
+++ Livrable 1/Livrable 1 PSI affichage Graphe/Livrable 1 PSI/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Livrable_1_PSI
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Graphe monGraphe = new Graphe();
+            string cheminMTX = "soc-karate.mtx";  
+            monGraphe.ChargerDepuisMTX(cheminMTX);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new VisualisationGraph(monGraphe));
+        }
+    }
+
+    internal class Noeud
+    {
+        public int IdNoeud { get; set; }
+        public List<int> Voisins { get; set; }
+        public Point Position { get; set; }  
+
+        public Noeud(int idNoeud)
+        {
+            IdNoeud = idNoeud;
+            Voisins = new List<int>();
+        }
+    }
+
+    internal class Graphe
+    {
+        public Dictionary<int, Noeud> Noeuds { get; set; } = new Dictionary<int, Noeud>();
+        public List<Tuple<int, int>> Liens { get; set; } = new List<Tuple<int, int>>();
+
+        public void AjouterLien(int antecedant, int suivant)
+        {
+            if (!Noeuds.ContainsKey(antecedant))
+                Noeuds[antecedant] = new Noeud(antecedant);
+            if (!Noeuds.ContainsKey(suivant))
+                Noeuds[suivant] = new Noeud(suivant);
+
+            Noeuds[antecedant].Voisins.Add(suivant);
+            Noeuds[suivant].Voisins.Add(antecedant);
+            Liens.Add(new Tuple<int, int>(antecedant, suivant));
+        }
+
+        public void ChargerDepuisMTX(string cheminFichier)
+        {
+            using (StreamReader sr = new StreamReader(cheminFichier))
+            {
+                string ligne;
+                bool lectureArêtes = false;
+
+                while ((ligne = sr.ReadLine()) != null)
+                {
+                    if (ligne.StartsWith("%")) 
+                        continue;
+
+                    string[] parties = ligne.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (!lectureArêtes)
+                    {
+                        lectureArêtes = true;
+                    }
+                    else if (parties.Length == 2)
+                    {
+                        int u = int.Parse(parties[0]);
+                        int v = int.Parse(parties[1]);
+                        AjouterLien(u, v);
+                    }
+                }
+            }
+
+           
+            int centerX = 400, centerY = 300, rayon = 200;
+            int nbNoeuds = Noeuds.Count;
+            int i = 0;
+
+            foreach (var noeud in Noeuds.Values)
+            {
+                double angle = (2 * Math.PI * i) / nbNoeuds;
+                noeud.Position = new Point(
+                    centerX + (int)(rayon * Math.Cos(angle)),
+                    centerY + (int)(rayon * Math.Sin(angle))
+                );
+                i++;
+            }
+        }
+    }
+
+    internal class VisualisationGraph : Form
+    {
+        private Graphe _graphe;
+
+        public VisualisationGraph(Graphe graphe)
+        {
+            _graphe = graphe;
+            this.Text = "Visualisation du Graphe";
+            this.Size = new Size(800, 600);
+            this.Paint += new PaintEventHandler(DessinerGraphe);
+        }
+
+        private void DessinerGraphe(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Yellow, 1);
+            Brush brush = new SolidBrush(Color.Blue);
+            Font font = new Font("Arial", 7);
+
+         
+            foreach (var lien in _graphe.Liens)
+            {
+                Noeud fromNode = _graphe.Noeuds[lien.Item1];
+                Noeud toNode = _graphe.Noeuds[lien.Item2];
+                g.DrawLine(pen, fromNode.Position, toNode.Position);
+            }
+
+            
+            foreach (var noeud in _graphe.Noeuds.Values)
+            {
+                g.FillEllipse(brush, noeud.Position.X - 10, noeud.Position.Y - 10, 20, 20);
+                g.DrawString(noeud.IdNoeud.ToString(), font, Brushes.White, noeud.Position.X - 5, noeud.Position.Y - 5);
+            }
+        }
+    }
+}
---
+++ Livrable 1/Livrable 1 PSI étude du Graphe/Livrable 1 mars 2025/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Livrable_1_PSI
+{
+    internal static class Programme
+    {
+        static void Main()
+        {
+            Graphe monGraphe = new Graphe();
+            string cheminMTX = "soc-karate.mtx";
+            monGraphe.ChargerDepuisMTX(cheminMTX);
+
+            Console.WriteLine("Matrice d'adjacence :");
+            monGraphe.AfficherMatriceAdjacence();
+            Console.WriteLine();
+
+            Console.WriteLine("Liste d'adjacence :");
+            monGraphe.AfficherListeAdjacence();
+            Console.WriteLine();
+
+            Console.WriteLine("\nParcours en largeur (BFS) à partir du sommet 1 :");
+            monGraphe.ParcoursLargeurBFS(1);
+
+            Console.WriteLine("\nParcours en profondeur (DFS) à partir du sommet 1 :");
+            monGraphe.ParcoursProfondeurDFS(1);
+
+            Console.WriteLine("\nLe graphe est-il connecté ? " + (monGraphe.EstConnecte() ? "Oui" : "Non"));
+            Console.WriteLine("Le graphe contient-il un cycle ? " + (monGraphe.ContientCycle() ? "Oui" : "Non"));
+            Console.WriteLine("Ordre du graphe : " + monGraphe.OrdreGraphe());
+            Console.WriteLine("Taille du graphe : " + monGraphe.TailleGraphe());
+
+            Console.WriteLine("\nAppuyez sur une touche pour quitter...");
+            Console.ReadKey();
+        }
+    }
+
+    internal class Noeud
+    {
+        public int Id { get; set; }
+        public List<int> Voisins { get; set; } = new List<int>();
+
+        public Noeud(int id)
+        {
+            Id = id;
+        }
+    }
+
+    internal class Graphe
+    {
+        public Dictionary<int, Noeud> Noeuds { get; set; } = new Dictionary<int, Noeud>();
+
+        public void AjouterLien(int precedent, int suivant)
+        {
+            if (!Noeuds.ContainsKey(precedent))
+                Noeuds[precedent] = new Noeud(precedent);
+            if (!Noeuds.ContainsKey(suivant))
+                Noeuds[suivant] = new Noeud(suivant);
+            Noeuds[precedent].Voisins.Add(suivant);
+            Noeuds[suivant].Voisins.Add(precedent);
+        }
+
+        public void AfficherListeAdjacence()
+        {
+            foreach (var noeud in Noeuds.OrderBy(n => n.Key))
+            {
+                noeud.Value.Voisins.Sort();
+                Console.WriteLine($"{noeud.Key} : {string.Join(", ", noeud.Value.Voisins)}");
+            }
+        }
+
+        public void AfficherMatriceAdjacence()
+        {
+            int n = Noeuds.Keys.Max();
+            int[,] matrice = new int[n + 1, n + 1];
+
+            foreach (var noeud in Noeuds)
+                foreach (var voisin in noeud.Value.Voisins)
+                    matrice[noeud.Key, voisin] = 1;
+
+            Console.Write("   ");
+            for (int i = 1; i <= n; i++)
+                Console.Write($"{i,3}");
+            Console.WriteLine();
+
+            Console.Write("   ");
+            for (int i = 1; i <= n; i++)
+                Console.Write("---");
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"{i,2} | ");
+                for (int j = 1; j <= n; j++)
+                    Console.Write($"{matrice[i, j],3}");
+                Console.WriteLine();
+            }
+        }
+
+        public void ParcoursLargeurBFS(int sommetDepart)
+        {
+            HashSet<int> visite = new HashSet<int>();
+            Queue<int> file = new Queue<int>();
+            file.Enqueue(sommetDepart);
+            visite.Add(sommetDepart);
+            while (file.Count > 0)
+            {
+                int noeud = file.Dequeue();
+                Console.Write(noeud + " ");
+                foreach (int voisin in Noeuds[noeud].Voisins)
+                {
+                    if (visite.Add(voisin))
+                        file.Enqueue(voisin);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void ParcoursProfondeurDFS(int sommetDepart)
+        {
+            HashSet<int> visite = new HashSet<int>();
+            Stack<int> pile = new Stack<int>();
+            pile.Push(sommetDepart);
+            while (pile.Count > 0)
+            {
+                int noeud = pile.Pop();
+                if (visite.Add(noeud))
+                {
+                    Console.Write(noeud + " ");
+                    foreach (int voisin in Noeuds[noeud].Voisins)
+                        pile.Push(voisin);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public bool EstConnecte()
+        {
+            if (Noeuds.Count == 0) return false;
+            HashSet<int> visite = new HashSet<int>();
+            DFSHelper(Noeuds.Keys.First(), visite);
+            return visite.Count == Noeuds.Count;
+        }
+
+        private void DFSHelper(int noeud, HashSet<int> visite)
+        {
+            if (visite.Add(noeud))
+                foreach (int voisin in Noeuds[noeud].Voisins)
+                    DFSHelper(voisin, visite);
+        }
+
+        public bool ContientCycle()
+        {
+            HashSet<int> visite = new HashSet<int>();
+            return Noeuds.Keys.Any(noeud => !visite.Contains(noeud) && CycleDFS(noeud, -1, visite));
+        }
+
+        private bool CycleDFS(int noeud, int parent, HashSet<int> visite)
+        {
+            visite.Add(noeud);
+            foreach (int voisin in Noeuds[noeud].Voisins)
+            {
+                if (!visite.Contains(voisin))
+                {
+                    if (CycleDFS(voisin, noeud, visite)) return true;
+                }
+                else if (voisin != parent)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int OrdreGraphe() => Noeuds.Count;
+
+        public int TailleGraphe() => Noeuds.Sum(n => n.Value.Voisins.Count) / 2;
+
+        public void ChargerDepuisMTX(string cheminFichier)
+        {
+            foreach (var ligne in File.ReadLines(cheminFichier).Where(l => !l.StartsWith("%")))
+            {
+                var parties = ligne.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parties.Length == 2)
+                    AjouterLien(int.Parse(parties[0]), int.Parse(parties[1]));
+            }
+        }
+    }
+}
+
+
